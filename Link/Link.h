5bfd1f92bd36f62b892771398a9ae61b6b5d919f
Jeremy Tregunna <jeremy@tregunna.ca>
2016-02-24 21:50:46 -0600

    Initial Commit

---
+++ Link/Link.h
@@ -0,0 +1,19 @@
+//
+//  Link.h
+//  Link
+//
+//  Created by Jeremy Tregunna on 2016-02-24.
+//  Copyright © 2016 Jeremy Tregunna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Link.
+FOUNDATION_EXPORT double LinkVersionNumber;
+
+//! Project version string for Link.
+FOUNDATION_EXPORT const unsigned char LinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Link/PublicHeader.h>
+
+
